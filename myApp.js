4af5ff172a5ffcb2253bad5de88ad1ad569f5930
Thanh <tnkchaseme@gmail.com>
2022-08-15 20:37:57 -0500

    connect to mongo db

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 var mongoose = require('mongoose')
-mongoose.connect(env.process.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
