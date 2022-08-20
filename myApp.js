c63537729dddc7fa789e9c7bf7dc8b4712180cbf
Thanh <tnkchaseme@gmail.com>
2022-08-19 21:40:02 -0500

    t

--- myApp.js
+++ myApp.js
@@ -73,8 +73,14 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    {name: personName},
+    {age: ageToSet}, function (err, data){
+      if(err) return console.log(err);
+      done(null, data)
+    }
+  )
+  
 };
 
 const removeById = (personId, done) => {
