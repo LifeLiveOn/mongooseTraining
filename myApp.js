7ff5cb192752170cd3d252270689d790d1f6c793
Thanh <tnkchaseme@gmail.com>
2022-08-19 22:01:22 -0500

    t

--- myApp.js
+++ myApp.js
@@ -103,8 +103,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch})
+  .sort({ name: -1 })
+  .limit(2)
+  .select({ name: 1,age:0,favoriteFoods:1 })
+  .exec(function(error, people) {
+    if(error) return console.log(err);
+    done(null,people)
+  });
 };
 
 /** **Well Done !!**
