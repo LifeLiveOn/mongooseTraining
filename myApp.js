8c546620b80c28ad78cd69c6397520faf2128717
Thanh <tnkchaseme@gmail.com>
2022-08-19 20:43:27 -0500

    t

--- myApp.js
+++ myApp.js
@@ -44,7 +44,10 @@ var findPeopleByName = function(personName, done) {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({FavoriteFoods:food},function (err,person){
+    if(err) return console.log(err);
+    done(null,person)
+  })
 };
 
 const findPersonById = (personId, done) => {
